839e21bb48f017a39d3506f965d8fee12a7eb09a
BlueEyesDev <84388093+BlueEyesDev@users.noreply.github.com>
2022-09-26 11:00:28 +0200

    Create Exemple.js

---
+++ Exemple.js
@@ -0,0 +1,5 @@
+//unminify
+console.log("hello"); console.log("world");//Return hello\r\nworld
+//minify
+console.log("hello") console.log("world")
+//ERROR: Uncaught SyntaxError: Unexpected identifier 'console'

42531aadd866f6f205fc57769dbcf2bb541d3954
BlueEyesDev <84388093+BlueEyesDev@users.noreply.github.com>
2022-10-18 21:16:42 +0200

    Create Exemple.js

--- Exemple.js
+++ Exemple.js
@@ -1,18 +1,4 @@
-//unminify
-console.log("hello"); console.log("world");//Return hello\r\nworld
-//minify
-console.log("hello") console.log("world")
-//ERROR: Uncaught SyntaxError: Unexpected identifier 'console'
-
-
-//Regex
-let b = 'bonjour, comment tu va guaca_fly ?'
-/bonjour/g.exec(b)
-//ERROR Uncaught ReferenceError: bonjour is not defined at <anonymous>:2:2
-
-//Replace
-let phrase = 'les points-virgules est '
-let fin = 'complexe'
-phrase += fin
-(phrase).replace('complexe', 'simple')
-//ERROR Uncaught TypeError: fin is not a function at <anonymous>:3:11
+let phrase = "Sans les points virgule il y a pas d'erreur"
+let fin = ' Ou pas ';
+phrase += fin;
+(phrase).replace('pas', 'des Possibilité')

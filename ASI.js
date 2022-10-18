e9f23e00270ff11ef776cb7cdbe3619ef497d820
BlueEyesDev <84388093+BlueEyesDev@users.noreply.github.com>
2022-10-18 21:20:09 +0200

    Update ASI.js

--- ASI.js
+++ ASI.js
@@ -2,3 +2,10 @@ let phrase = "Sans les points virgule il y a pas d'erreur"
 let fin = ' Ou pas'
 phrase += fin
 (phrase).replace('pas', 'des Possibilité')
+//Uncaught TypeError: fin is not a function at <anonymous>:3:11
+
+let phrase = "Sans les points virgule il y a pas d'erreur";
+let fin = ' Ou pas ';
+phrase += fin;
+(phrase).replace('pas', 'des Possibilité');
+//"Sans les points virgule il y a des Possibilité d'erreur Ou pas"
